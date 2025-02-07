c82d6d0e21b58dbd37febc5899fbdb0343578eb8
simorai <109289730+simorai@users.noreply.github.com>
2025-02-07 11:20:53 +0000

    Adding transparency to player object

--- script.js
+++ script.js
@@ -17,8 +17,17 @@ window.addEventListener("load", function () {
     }
     draw(context) {
       context.beginPath();
-      context.arc(200, 300, 50, 0, Math.PI * 2);
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      );
+      context.save();
+      context.globalAlpha = 0.5;
       context.fill();
+      context.restore();
       context.Stroke();
     }
   }
